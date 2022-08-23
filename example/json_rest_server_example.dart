81d02d24e37b44fcd9c341252709a51ee4d7a413
Rodrigo Rahman <rodrigorahman@academiadoflutter.com.br>
2022-08-22 23:20:10 -0300

    init project

---
+++ example/json_rest_server_example.dart
@@ -0,0 +1,6 @@
+import 'package:json_rest_server/json_rest_server.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/json_rest_server.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library json_rest_server;
+
+export 'src/json_rest_server_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
