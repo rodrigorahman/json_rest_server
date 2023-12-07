70a66852fc1fc1c5210cef207085e40d7ea6c1a4
Rodrigo Rahman <rodrigorahman@academiadoflutter.com.br>
2023-12-07 00:33:50 -0300

    feat: Migration post to Dart 3

--- lib/src/server/handlers/post_handler.dart
+++ lib/src/server/handlers/post_handler.dart
@@ -8,9 +8,7 @@ import '../../repositories/database_repository.dart';
 class PostHandler {
   final _databaseRepository = GetIt.I.get<DatabaseRepository>();
   Future<Map<String, dynamic>?> execute(Request request) async {
-    final segments = request.url.pathSegments;
-
-    final table = segments.first;
+    final Uri(pathSegments: [table]) = request.url;
 
     if (_databaseRepository.tableExists(table)) {
       var body = await request.readAsString();
